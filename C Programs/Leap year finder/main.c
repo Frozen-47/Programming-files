dc7cb187d00539e2460f72fb6b798c23deb30a93
Sabareesh G <sabareeshgm47@gmail.com>
2025-02-20 15:39:12 +0530

    Delete C Programs directory

--- C Programs/Leap year finder/main.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a;
-    printf("ENTER A YEAR:");
-    scanf("%d",&a);
-    if(a%4==0){printf("THE YEAR IS LEAP YEAR.");}
-    else{printf("THE YEAR IS NOT LEAP YEAR.");}
-    return 0;
-}
